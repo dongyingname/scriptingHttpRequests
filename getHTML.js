5dba9d865aceca8330017bb881ffe6c18f83a40c
Ying Dong <dongyingname@yahoo.com>
2018-10-29 20:59:26 +0000

    fixed minor issue in step 4

--- getHTML.js
+++ getHTML.js
@@ -1,10 +1,10 @@
 function getHTML (options, callback) {
-
   /* Add your code here */
   var https = require('https');
   https.get(options, function (response) {
     // set encoding of received data to UTF-8
     response.setEncoding('utf8');
+    //print with callback
     response.on('data', callback);
     });
 }
@@ -15,4 +15,4 @@ var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step4.html'
 };
-console.log(getHTML(requestOptions, printHTML));
+getHTML(requestOptions, printHTML);
