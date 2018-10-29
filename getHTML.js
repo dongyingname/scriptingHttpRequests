2ec490c8877cc63b3ef8de618c49cd7b74dee394
Ying Dong <dongyingname@yahoo.com>
2018-10-29 20:54:58 +0000

    finnaly I called backgit add .! finished step 4

--- getHTML.js
+++ getHTML.js
@@ -1,23 +1,18 @@
 function getHTML (options, callback) {
 
   /* Add your code here */
-  var input = process.argv.splice(2)[0];
   var https = require('https');
-  var str = '';
   https.get(options, function (response) {
     // set encoding of received data to UTF-8
     response.setEncoding('utf8');
-    response.on('data', function (data) {
-      str += data + '\n';
-      return printHTML;
-    }
-  }
-
+    response.on('data', callback);
+    });
+}
 function printHTML (html) {
   console.log(html);
 }
-
 var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step4.html'
 };
+console.log(getHTML(requestOptions, printHTML));
