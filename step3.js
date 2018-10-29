64e13e0becabe691a270848db3c6a619f0ec1201
Ying Dong <dongyingname@yahoo.com>
2018-10-29 20:29:00 +0000

    finished step 3

--- step3.js
+++ step3.js
@@ -1,10 +1,25 @@
+var input = process.argv.splice(2)[0];
+var https = require('https');
 function getAndPrintHTML (options) {
+  // 'sytantris.github.io/http-examples/step1.html'
+  var str= '';
+  // var requestOptions = {
+  //   host: 'sytantris.github.io',
+  //   path: '/http-examples/step1.html'
+  // };
+  // /* Add your code here */
+  https.get(options, function (response) {
+    // set encoding of received data to UTF-8
+    response.setEncoding('utf8');
 
-  /* Add your code here */
-
+    // the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+      //console.log('data', data);
+      str += data + '\n';
+    });
+    response.on('end', function(){
+      console.log(str);
+    });
+  });
 }
-
-var requestOptions = {
-  host: 'sytantris.github.io',
-  path: '/http-examples/step3.html'
-};
+getAndPrintHTML(input);
